7fdbdedcf530596202acf78c52bad50c0040ad0e
VJS <minsung_96@naver.com>
2023-03-19 00:00:01 +0900

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav
@@ -37,21 +38,50 @@ const Navbar = () => {
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map(link) => (
+          {navLinks.map((link) => (
             <li
               key={link.id}
               className={`${
                 active === link.title
                 ? "text-white"
                 : "text-secondary"
-              }`}
+              } hover:text-white font-medium cursor-pointer text-[18px]`}
+              onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>
                 {link.title}
               </a>
             </li>
-          )}
+          ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient top-20 right-0 mx-4 my-2 min-w-[140px] absolute z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end flex-col gap-4">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } font-poppins font-medium cursor-pointer text-[16px]`}
+                onClick={() => setActive(link.title)}
+              >
+                <a href={`#${link.id}`}>
+                  setToggle(!toggle)
+                  {link.title}
+                </a>
+              </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
