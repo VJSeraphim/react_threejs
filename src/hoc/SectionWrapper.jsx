5bceea27823cdefc6f58d18e5320c85292a7da65
VJS <minsung_96@naver.com>
2023-03-24 23:38:14 +0900

    Experience Section + Tech Balls

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,6 +13,9 @@ const SectionWrapper = ( Component, idName ) => function HOC() {
             viewport={{ once: true, amount: 0.25 }}
             className={`${styles.padding} mx-auto relative z-0 max-w-7xl`}
         >
+            <span className="hash-span" id={idName}>
+                %nbsp;
+            </span>
             <Component />
         </motion.section>
     )
